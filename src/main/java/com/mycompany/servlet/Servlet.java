5e28f6c26d8023c8f02741e818f87d95ec601242
Robson Cruz <ghost0092@gmail.com>
2017-12-14 16:17:24 -0200

    Integracao inicial realizada

---
+++ src/main/java/com/mycompany/servlet/Servlet.java
@@ -0,0 +1,27 @@
+package com.mycompany.servlet;
+
+import com.mycompany.controller.Controller;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter pw = res.getWriter();
+        res.setContentType("text/html");
+
+        String n1 = req.getParameter("n1");
+        String n2 = req.getParameter("n2");
+
+        Controller controller = new Controller();
+        controller.cadastrarPessoa(n1, n2);
+
+    }
+
+    
+}
