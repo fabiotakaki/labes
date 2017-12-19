b7e0301ddb63a75e287e22c971d4929e0398a525
Robson Cruz <ghost0092@gmail.com>
2017-12-19 19:27:12 -0200

    Modificado o método listarExperimentos na persistência para correção de erros.

--- src/main/java/com/mycompany/persistences/ExperimentoPersistence.java
+++ src/main/java/com/mycompany/persistences/ExperimentoPersistence.java
@@ -5,40 +5,28 @@
  */
 package com.mycompany.persistences;
 
+import com.mycompany.configs.HibernateUtil;
 import com.mycompany.model.Experimento;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author Rogerio
  */
 public class ExperimentoPersistence {
+
     private static final Logger LOGGER = Logger.getLogger(ExperimentoPersistence.class.getName());
 
-    public static List<Experimento> listarExperimentos() {
-       SessionFactory factory = HibernateUtil.getSessionFactory();
-        Session session = factory.openSession();
-        boolean commited = false;
-        Transaction t = null;
-        try {
-            t = session.beginTransaction();
-            Query query = session.createQuery("from Experimento");
-            List<Experimento> list = query.list();
-            t.commit();
-            commited = true;
-            return list;
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "ERRO: [{0}]", e.getMessage());
-            if (t != null && !t.wasCommitted()) {
-                t.rollback();
-            }
-        } finally {
-            session.close();
-        }
-        return commited;
+    private ExperimentoPersistence() {
+        // default constructor
     }
-
-
 
     public static boolean save(Experimento experimento) {
         SessionFactory factory = HibernateUtil.getSessionFactory();
@@ -55,10 +43,28 @@ public class ExperimentoPersistence {
             if (t != null && !t.wasCommitted()) {
                 t.rollback();
             }
+            throw e;
         } finally {
             session.close();
         }
         return commited;
+    }
+
+    public static List<Experimento> listarExperimentos() {
+        SessionFactory factory = HibernateUtil.getSessionFactory();
+        Session session = factory.openSession();
+        List<Experimento> listaExperimentos;
+        try {
+            Query query = session.createQuery("from Experimento");
+            List resultList = query.list();
+            listaExperimentos = (List<Experimento>) resultList;
+        } catch (HibernateException e) {
+            LOGGER.log(Level.SEVERE, "ERRO: [{0}]", e.getMessage());
+            throw e;
+        } finally {
+            session.close();
+        }
+        return listaExperimentos;
     }
 
 }
