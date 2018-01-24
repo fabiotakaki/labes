16eede98e4dc0c61fc630429fc78742149f08414
felipepbcorrea <felipepbcorrea@users.noreply.github.com>
2018-01-24 16:20:54 -0200

    Refinamento de Testes
    Testes foram adptados as novas verificações dos Controllers.

--- src/test/java/Experimento/ListarExperimento.java
+++ src/test/java/Experimento/ListarExperimento.java
@@ -6,7 +6,10 @@
 package Experimento;
 
 import com.mycompany.controller.ControllerExperimento;
+import com.mycompany.controller.ControllerUsuario;
 import com.mycompany.model.Experimento;
+import com.mycompany.model.Usuario;
+import java.util.Calendar;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -23,6 +26,10 @@ public class ListarExperimento {
     
     public ListarExperimento() {
     }
+      
+    Usuario usuario;
+    Calendar calendario;
+    Experimento experimento;
     
     @BeforeClass
     public static void setUpClass() {
@@ -32,8 +39,27 @@ public class ListarExperimento {
     public static void tearDownClass() {
     }
     
+    //Caso o banco esteja vazio, cria usuario de teste com 2 experimentos testes
     @Before
     public void setUp() {
+        calendario = Calendar.getInstance();
+        usuario = ControllerUsuario.buscaUsuario(1);
+        
+        if(ControllerUsuario.buscaUsuario(1) == null){
+           usuario = new Usuario("UsuarioTeste@teste.com","123456789");
+           usuario.saveOnDatabase();
+        }
+        
+        if(ControllerExperimento.listarExperimentos(1).size() == 0){
+            Experimento experimento1 = new Experimento("Testeum", "Testeum", false, calendario, false, usuario);
+            experimento1.saveOnDatabase();
+            Experimento experimento2 = new Experimento("Testedois", "Testedois", false, calendario, false, usuario);
+            experimento2.saveOnDatabase();
+        }    
+        else if(ControllerExperimento.listarExperimentos(1).size() == 1){
+            Experimento experimento2 = new Experimento("Testedois", "Testedois", false, calendario, false, usuario);
+            experimento2.saveOnDatabase();
+        }
     }
     
     @After
